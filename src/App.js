ed5a41270763e1d56e31a02399ec7b12e60788fb
LarsM <lam@cphbusiness.dk>
2017-12-19 14:55:33 +0100

    Start code for exercise

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import {
+  HashRouter as Router,Route, Link, NavLink} from 'react-router-dom'
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+
+class Jokes extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  componentDidMount() {
+    //Could be a place to fethc data, WHY ??
+  }
+  render() {
+    return (
+      <div>
+        <p>Change this view to show all jokes (only show the setup property) </p>
+        <ul>
+        </ul>
+      </div>
+    )
+  }
+}
+
+const App = () => (
+  <Router>
+    <div>
+      <ul className="header">
+        <li><NavLink exact to="/">Home</NavLink></li>
+        <li><NavLink to="/jokes">Jokes </NavLink></li>
+      </ul>
+      <hr />
+      <switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/jokes" component={Jokes} />
+      </switch>
+    </div>
+  </Router>
+)
+export default App;
